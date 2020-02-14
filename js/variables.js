7b819d39e2db68a6b16aaa45822879005fe23d42
Abhishek Kadam <abhimkdm@gmail.com>
2020-02-14 20:24:12 +0530

    Variables in TypeScript

---
+++ js/variables.js
@@ -0,0 +1,8 @@
+"use strict";
+//var, let & const
+var message = "Hello TypeScript";
+console.log(message);
+var message02 = "Hello TypeScript 02";
+console.log(message02);
+var message03 = "Hello TypeScript 02";
+console.log(message03);
---
+++ variables.ts
@@ -0,0 +1,18 @@
+//var, let & const
+var message = "Hello TypeScript";
+console.log(message);
+
+//Any Type
+let message02;
+message02 = "Hello TypeScript 02";
+message02 = 1234;
+console.log(message02);
+
+///Strongly Type
+let message04: number;
+//message04 = "Hello TypeScript 02";
+message04 = 1234;
+console.log(message04);
+
+const message03 = "Hello TypeScript 02";
+console.log(message03);
